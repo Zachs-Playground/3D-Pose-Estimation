f54e7658c18093ef64cd3f28dc9ce4189352c38c
Zach Wang <34903333+ZachWang-ComputerVision@users.noreply.github.com>
2023-03-04 22:14:45 -0800

    update

--- model.py
+++ model.py
@@ -3,25 +3,20 @@ import torch.nn as nn
 import torchvision
 
 class Pred3DPose(nn.Module):
-    def __init__(self, n_dim=4, n_point_cluster=8, n_joint=13):
+    def __init__(self, n_dim=4, n_point_cluster=8, n_joint=13, sequence=5):
         super(Pred3DPose, self).__init__()
         
         self.n_joint = n_joint
-        # self.n_features = 100
+        self.span = sequence // 2
+        self.n_features = 100
 
-        # init_channels = [20, 60, self.n_features]
-        # cat_channels = [16, 32, 1]
-        # spatial_channels = [n_joint * 2, n_joint * 3, n_joint]
-        # temporal_channels = [init_channels[-1] * 2, init_channels[-1] * 3, init_channels[-1]]
-        # pred_channels = [init_channels[-1] * 3, init_channels[-1], init_channels[-1] // 2, 3]
+        init_channels = [20, 60, self.n_features]
+        cat_channels = [16, 32, 1]
+        spatial_channels = [n_joint * 2, n_joint * 3, n_joint]
+        temporal_channels = [init_channels[-1] * 2, init_channels[-1] * 3, init_channels[-1]]
+        spatial_channels_2 = [(sequence - self.span) * n_joint * 2, (sequence - self.span) * n_joint * 3, (sequence - self.span) * n_joint]
+        pred_channels = [init_channels[-1] * 3, init_channels[-1], init_channels[-1] // 2, 3]
 
-        self.n_features = 200
-
-        init_channels = [20, 60, 100, self.n_features]
-        cat_channels = [16, 32, 8, 1]
-        spatial_channels = [n_joint * 2, n_joint * 3, n_joint * 2, n_joint]
-        temporal_channels = [init_channels[-1] * 2, init_channels[-1] * 3, init_channels[-1] * 2, init_channels[-1]]
-        pred_channels = [init_channels[-1] * 3, init_channels[-1] * 2, init_channels[-1], init_channels[-1] // 2, 3]
 
         self.mlp_init = torchvision.ops.MLP(n_dim, hidden_channels = init_channels, norm_layer = nn.LayerNorm, 
                                             activation_layer = nn.ReLU, inplace=False, bias = True, dropout = 0.1)
@@ -41,8 +36,26 @@ class Pred3DPose(nn.Module):
         self.trajectory = torchvision.ops.MLP(2 * init_channels[-1], hidden_channels = temporal_channels, norm_layer = nn.LayerNorm, 
                                             activation_layer = nn.ReLU, inplace=False, bias = True, dropout = 0.1)
         
+        self.mlp_spatial_2 = torchvision.ops.MLP((sequence - self.span) * n_joint, hidden_channels = spatial_channels_2, norm_layer = nn.LayerNorm, 
+                                            activation_layer = nn.ReLU, inplace=False, bias = True, dropout = 0.1)
+        
         self.pred = torchvision.ops.MLP(3 * init_channels[-1], hidden_channels = pred_channels, norm_layer = nn.LayerNorm, 
                                             activation_layer = nn.ReLU, inplace=False, bias = True, dropout = 0.1)
+        
+    #     self.init_weights()
+
+
+    # def init_weights(self):
+    #     def weight_init(m):
+    #         if isinstance(m, nn.Conv2d):
+    #             nn.init.kaiming_normal_(m.weight)
+    #         if getattr(m, 'bias') is not None:
+    #             nn.init.constant_(m.bias, 0)
+    #         if isinstance(m, nn.ModuleList):
+    #             for layer in m:
+    #                 # linear = layer[0]
+    #                 nn.init.kaiming_normal_(layer[0].weight)
+    #     self.apply(weight_init)
                                             
     def _get_motion(self, a, b):
         for joint_idx in range(a.size(0)):
@@ -51,8 +64,8 @@ class Pred3DPose(nn.Module):
                 b[joint_idx] = torch.zeros_like(b[joint_idx])
         return b - a
     
-    def forward(self, x, span):
-        n_frames = x.size(0) - span * 2
+    def forward(self, x):
+        n_frames = x.size(0) - self.span * 2
         x = self.mlp_init(x)                # shape => frames x joints x cluster points x feature size
         x = torch.transpose(x, -1, -2)      # shape => frames x joints x feature size x cluster points
         x = self.mlp_cat_points(x)          # shape => frames x joints x feature size x 1
@@ -61,19 +74,19 @@ class Pred3DPose(nn.Module):
         x = self.mlp_spatial(x)             # shape => frames x feature size x joints
         x = torch.transpose(x, -1, -2)      # shape => frames x joints x feature size
         x = self.process_features(x)
-        
-        target_frames = x[span : n_frames + span]
-        previous_t1_frames = x[span - 1 : n_frames + span - 1]
-        next_t1_frames = x[span + 1 : n_frames + span + 1]
+
+        target_frames = x[self.span : n_frames + self.span]
+        previous_t1_frames = x[self.span - 1 : n_frames + self.span - 1]
+        next_t1_frames = x[self.span + 1 : n_frames + self.span + 1]
         forward_motion = torch.ones_like(target_frames)
         backward_motion = torch.ones_like(target_frames)
         out = torch.zeros(n_frames, self.n_joint, self.n_features, dtype=torch.float32)
-        for i in range(span, n_frames + span):
+        for i in range(self.span, n_frames + self.span):
             forward_flow = self._get_motion(x[i - 2], x[i - 1])
-            forward_motion[i - span] = forward_flow
-            backward_flow = self._get_motion(x[i + 1], x[i + 2])
-            backward_motion[i - span] = backward_flow
-        
+            forward_motion[i - self.span] = forward_flow
+            backward_flow = self._get_motion(x[i + 2], x[i + 1])
+            backward_motion[i - self.span] = backward_flow
+
         forward_motion = self.motion(forward_motion)
         backward_motion = self.motion(backward_motion)
 
@@ -83,6 +96,10 @@ class Pred3DPose(nn.Module):
         forward_pred = self.trajectory(forward_pred)
         backward_pred = self.trajectory(backward_pred)
 
-        out = torch.cat((forward_pred, target_frames, backward_pred), 2)
+        out = torch.cat((forward_pred, target_frames, backward_pred), 1)
+        out = torch.transpose(out, -2,-1)
+        out = self.mlp_spatial_2(out)
+        out = torch.transpose(out, -2,-1)
+        out = torch.cat((out[:, 0:13,:], out[:, 13:26,:], out[:, 26:39,:]), 2)
         out = self.pred(out)
         return out
---
+++ train.py
@@ -0,0 +1,123 @@
+""" Train """
+
+import math
+import json
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+
+from util import prepare_inputs
+from model import Pred3DPose
+
+cuda = True
+load_pretrained = True
+device = torch.device("cuda" if torch.cuda.is_available() and cuda == True else "cpu")
+print("The coda is running on: ", device)
+epoch = 500
+batch_size = 31
+frame_sequence = 5
+if frame_sequence % 2 == 0:
+  raise RuntimeError("frame_sequence must be an odd number.")
+
+span = frame_sequence // 2
+
+
+cam_path = "./data/processed_data/cam.json"    
+annot_3d_path = "./data/processed_data/annot_3d.json"   
+annot_2d_path = "./data/processed_data/annot_2d.json"   
+
+
+# ---------------------------------------------------------
+# data section
+# ---------------------------------------------------------
+with open(cam_path, "r") as camf:
+  cam_params = json.loads(camf.read())
+  cam_params_torch = torch.tensor(cam_params[0], dtype=torch.float32)   #  4cams
+  cam_params_torch = torch.squeeze(cam_params_torch, dim=1)
+  cam_params_dict = cam_params[1]
+
+with open(annot_3d_path, "r") as annot3df:
+  labels_3d = json.loads(annot3df.read())   # person, video, frame, joints, xyz
+  test_labels_3d = labels_3d[-1]
+  labels_3d = labels_3d[:-1]
+
+with open(annot_2d_path, "r") as annot2df:
+  labels_2d = json.loads(annot2df.read())   # person, camera, video, frame, joints, xy
+  test_labels_2d = labels_2d[-1]
+  labels_2d = labels_2d[:-1]
+
+
+model = Pred3DPose().to(device)
+model.train()
+criterion = nn.MSELoss()
+
+if load_pretrained:
+  checkpoint = torch.load("./checkpoint/epoch94.pt")
+  model.load_state_dict(checkpoint['model_state_dict'])
+  print("Loaded Checkpoint")
+
+optimizer = optim.Adam(model.parameters(), lr=0.0005)
+if load_pretrained:
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+
+
+for i in range(epoch):
+  avg_loss_per_epoch = 0.0
+  count = 0
+  for person_idx, person in enumerate(labels_2d):
+    
+    for vid_idx, vid in enumerate(person[0]):
+      mini_len =  math.inf
+      inputs = []
+      for cam_idx in range(len(person)):
+        frames_in_one_video = person[cam_idx][vid_idx]
+        mini_len = min(len(frames_in_one_video), mini_len)
+        inputs.append(frames_in_one_video)
+
+      for cam_input_idx, one_cam_input in enumerate(inputs):
+        if len(one_cam_input) > mini_len:
+          inputs[cam_input_idx] = one_cam_input[ : mini_len]
+      
+      gt_2d = torch.tensor(inputs, dtype=torch.float32)
+      # gt_2d = torch.squeeze(gt_2d, dim=1)
+      inputs = prepare_inputs(gt_2d, cam_params_torch)
+      
+      for idx in range(span, inputs.size(0) - span, batch_size):
+        optimizer.zero_grad()
+
+        small_batch_inputs = inputs[idx - span : idx + batch_size + span].to(device)
+
+        if (small_batch_inputs.size(0) - span) < 1 + span:
+          continue
+        else:
+          pred_3d = model(small_batch_inputs)
+          # pred_2d = project_3d_to_2d(pred_3d, cam_params_dict)
+          actual_n_frames = pred_3d.size(0)
+          # batch_gt_2d = gt_2d[:, idx : idx + actual_n_frames].to(device)
+          
+          batch_gt_3d = labels_3d[person_idx][vid_idx][idx : idx + actual_n_frames]
+          batch_gt_3d = torch.tensor(batch_gt_3d, dtype=torch.float32).to(device)
+          batch_gt_3d *= 100.0
+          # scale = align_3d_scales(pred_3d, batch_gt_3d)
+          # pred_3d *= scale
+          
+          # loss = calculate_loss(pred_2d, pred_3d, batch_gt_2d, batch_gt_3d, loss_func=criterion)
+          # loss = calculate_loss(pred_2d, batch_gt_2d, loss_func=criterion)
+          loss = criterion(pred_3d, batch_gt_3d)
+          avg_loss_per_epoch += loss
+          count += 1
+          
+          loss.backward()
+          optimizer.step()
+
+    # print(f"average loss for epoch: {i}, people: {person_idx}, loss: {avg_loss_per_vid / count}")
+  print(f"average loss for epoch: {i}, loss: {avg_loss_per_epoch / count}")
+  
+  torch.save({
+      'epoch': i,
+      'model_state_dict': model.state_dict(),
+      'optimizer_state_dict': optimizer.state_dict(),
+      'loss': loss,
+    }, f"./checkpoint/epoch{i}.pt")
